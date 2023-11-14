1155da328b497996ca3147d32dffb7700634a375
surajs <3ssalunke>
2023-11-14 23:20:57 +0530

    commit - #1311

---
+++ src/lib/supabase/db.ts
@@ -0,0 +1,24 @@
+import * as dotenv from "dotenv";
+import { drizzle } from "drizzle-orm/postgres-js";
+import postgres from "postgres";
+import * as schema from "../../../migrations/schema";
+import { migrate } from "drizzle-orm/postgres-js/migrator";
+dotenv.config({ path: "./env" });
+
+if (!process.env.DATABASE_URL) {
+  console.error("🔴 Database connection url not found");
+}
+
+const client = postgres(process.env.DATABASE_URL as string, { max: 1 });
+const db = drizzle(client, { schema });
+const migrateDB = async () => {
+  try {
+    console.log("🟠 Migrating client");
+    await migrate(db, { migrationsFolder: "migrations" });
+    console.log("🟢 Succefully migrated");
+  } catch (error) {
+    console.error("🔴 Error migrating client");
+  }
+};
+migrateDB();
+export default db;
